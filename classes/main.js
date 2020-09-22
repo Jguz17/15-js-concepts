7334d7d3c68903d03000402a162382a0f10cd779
Johnathan Guzman <johnathanguzman17@gmail.com>
2020-09-22 07:17:53 -0500

    finished with second example

--- classes/main.js
+++ classes/main.js
@@ -1,13 +1,30 @@
+// Method 1
+
+// class Cereal {
+//     constructor(name, milkType) {
+//         this.name = name,
+//         this.milkType = milkType
+//     }
+
+//     getCereal() {
+//         return `*Pouring a bowl of ${this.name}, with some ${this.milkType} milk*`
+//     }
+// }
+
+
+// Method 2
+
 class Cereal {
     constructor(name, milkType) {
         this.name = name,
         this.milkType = milkType
     }
-
-    getCereal() {
-        return `*Pouring a bowl of ${this.name}, with some ${this.milkType} milk*`
-    }
 }
+
+Cereal.prototype.getCereal = function() {
+    return `*Pouring a bowl of ${this.name}, with some ${this.milkType} milk*`
+}
+
 
 const frostedFlakes = new Cereal(
     'Frosted Flakes',
